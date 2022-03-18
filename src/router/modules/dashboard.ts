c6d6dc76693cea0fee61918e00785c0cfee9d003
yang <boyyang-love@outlook.com>
2022-03-18 15:15:30 +0800

    删除组件

--- src/router/modules/dashboard.ts
+++ src/router/modules/dashboard.ts
@@ -36,26 +36,7 @@ const routes: Array<RouteRecordRaw> = [
           permissions: ['dashboard_console'],
         },
         component: () => import('@/views/dashboard/console/console.vue'),
-      },
-      // {
-      //   path: 'monitor',
-      //   name: `${ routeName }_monitor`,
-      //   meta: {
-      //     title: '监控页',
-      //     permissions: ['dashboard_monitor']
-      //   },
-      //   component: () => import('@/views/dashboard/monitor/monitor.vue')
-      // },
-      {
-        path: 'workplace',
-        name: `${routeName}_workplace`,
-        meta: {
-          title: '工作台',
-          keepAlive: true,
-          permissions: ['dashboard_workplace'],
-        },
-        component: () => import('@/views/dashboard/workplace/workplace.vue'),
-      },
+      }
     ],
   },
 ];
